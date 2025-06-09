43857e54ac88c9fd87abd3782b43945351fb5f62
Akbar Anung <akbaranung06@gmail.com>
2025-06-09 16:10:57 +0700

    update

--- application/views/pages/pengajuan/v_pengajuan_form.php
+++ application/views/pages/pengajuan/v_pengajuan_form.php
@@ -46,36 +46,38 @@
             </div>
             <div class="row">
               <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <table class="table">
-                  <thead>
-                    <tr>
-                      <th>Uraian</th>
-                      <th>Qty</th>
-                      <th>Price</th>
-                      <th>Total</th>
-                      <th>#</th>
-                    </tr>
-                  </thead>
-                  <tbody id="uraian-pengajuan">
-                    <tr id="clone">
-                      <td><textarea name="uraian[]" id="uraian" class="form-control"></textarea></td>
-                      <td><input type="text" class="form-control qty" name="qty[]" id="qty"></td>
-                      <td><input type="text" class="form-control price" name="price[]" id="price"></td>
-                      <td><input type="text" class="form-control subtotal" name="subtotal[]" id="subtotal" readonly></td>
-                      <td>
-                        <span class="btn btn-success add-row btn-sm"><i class="fe fe-plus-square fe-12"></i></span>
-                        <span class="btn btn-danger hapus-row btn-sm"><i class="fe fe-trash-2 fe-12"></i></span>
-                      </td>
-                    </tr>
-                  </tbody>
-                  <tfoot>
-                    <tr>
-                      <td colspan="3" style="text-align: end;"><span>TOTAL</span></td>
-                      <td><input type="text" class="form-control total" name="total" id="total" readonly></td>
-                      <td></td>
-                    </tr>
-                  </tfoot>
-                </table>
+                <div class="table-responsive">
+                  <table class="table" style="min-width: 952px;">
+                    <thead class="thead-dark">
+                      <tr>
+                        <th>Uraian</th>
+                        <th width="80px">Qty</th>
+                        <th>Price</th>
+                        <th>Total</th>
+                        <th>#</th>
+                      </tr>
+                    </thead>
+                    <tbody id="uraian-pengajuan">
+                      <tr id="clone">
+                        <td><textarea name="uraian[]" id="uraian" class="form-control"></textarea></td>
+                        <td><input type="text" class="form-control qty" name="qty[]" id="qty"></td>
+                        <td><input type="text" class="form-control price" name="price[]" id="price"></td>
+                        <td><input type="text" class="form-control subtotal" name="subtotal[]" id="subtotal" readonly></td>
+                        <td>
+                          <span class="btn btn-success add-row btn-sm"><i class="fe fe-plus-square fe-12"></i></span>
+                          <span class="btn btn-danger hapus-row btn-sm"><i class="fe fe-trash-2 fe-12"></i></span>
+                        </td>
+                      </tr>
+                    </tbody>
+                    <tfoot>
+                      <tr>
+                        <td colspan="3" style="text-align: end;"><span>TOTAL</span></td>
+                        <td><input type="text" class="form-control total" name="total" id="total" readonly></td>
+                        <td></td>
+                      </tr>
+                    </tfoot>
+                  </table>
+                </div>
                 <div class="row">
                   <div class="col-lg-12 col-md-12 col-sm-12 col-12">
                     <a href="<?= site_url('pengajuan/list') ?>" class="btn btn-warning">Kembali</a>
--- application/views/pages/pengajuan/v_pengajuan_list.php
+++ application/views/pages/pengajuan/v_pengajuan_list.php
@@ -24,50 +24,55 @@
                     <button class="btn btn-secondary" type="submit">
                       Cari
                     </button>
-                    <a href="<?= site_url('pengajuan/list') ?>" class="btn btn-warning">Tampilkan Semua</a>
+                    <a href="<?= site_url('pengajuan/list') ?>" class="btn btn-warning">Reset</a>
                   </div>
                 </div>
               </form>
             </div>
           </div>
-          <table class="table table-hover table-sm">
-            <thead style="background-color:#3498db;">
-              <tr>
-                <th style="color: white;">No</th>
-                <th style="color: white;">No Rekening</th>
-                <th style="color: white;">Tanggal</th>
-                <th style="color: white;">Total</th>
-                <th style="color: white;">Posisi</th>
-                <th style="color: white;">#</th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php
-              if (empty($data_pengajuan)) { ?>
-                <tr>
-                  <td colspan="6" class="text-center">Data tidak ditemukan</td>
-                </tr>
-                <?php } else {
-                $nip = $this->session->userdata('nip');
-                foreach ($data_pengajuan as $data) {
-                ?>
+          <div class="table-responsive">
+            <div style="min-width: 951px;">
+              <table class="table table-hover" style="width: 100% !important;">
+                <thead style="background-color:#3498db;">
                   <tr>
-                    <td><?= $data->kode; ?></td>
-                    <td><?= $data->no_rekening ?></td>
-                    <td><?= date('d/m/y', strtotime($data->tanggal)) ?></td>
-                    <td><?= rupiah($data->total); ?></td>
-                    <td><?= $data->posisi; ?></td>
-                    <td>
-                      <?php if ($data->status == 0) { ?>
-                        <a href="<?= site_url('pengajuan/ubah/') . $data->kode ?>" class="btn btn-success btn-sm"><i class="fe fe-edit-3 fe-12"></i> Update</a>
-                      <?php } ?>
-                      <a href="<?= site_url('pengajuan/detail/') . $data->kode ?>" class="btn btn-warning btn-sm"><i class="fe fe-eye fe-12"></i> Detail</a>
-                    </td>
+                    <th style="color: white;">No</th>
+                    <th style="color: white;">No Rekening</th>
+                    <th style="color: white;">Tanggal</th>
+                    <th style="color: white;">Total</th>
+                    <th style="color: white;">Posisi</th>
+                    <th style="color: white;">#</th>
                   </tr>
-              <?php }
-              } ?>
-            </tbody>
-          </table>
+                </thead>
+                <tbody>
+                  <?php
+                  if (empty($data_pengajuan)) { ?>
+                    <tr>
+                      <td colspan="6" class="text-center">Data tidak ditemukan</td>
+                    </tr>
+                    <?php } else {
+                    $nip = $this->session->userdata('nip');
+                    foreach ($data_pengajuan as $data) {
+                    ?>
+                      <tr>
+                        <td><?= $data->kode; ?></td>
+                        <td><?= $data->no_rekening ?></td>
+                        <td><?= date('d/m/y', strtotime($data->tanggal)) ?></td>
+                        <td><?= rupiah($data->total); ?></td>
+                        <td><?= $data->posisi; ?></td>
+                        <td>
+                          <?php if ($data->status == 0) { ?>
+                            <a href="<?= site_url('pengajuan/ubah/') . $data->kode ?>" class="btn btn-success btn-sm"><i class="fe fe-edit-3 fe-12"></i> Update</a>
+                          <?php } ?>
+                          <a href="<?= site_url('pengajuan/detail/') . $data->kode ?>" class="btn btn-warning btn-sm"><i class="fe fe-eye fe-12"></i> Detail</a>
+                        </td>
+                      </tr>
+                  <?php }
+                  } ?>
+                </tbody>
+              </table>
+            </div>
+          </div>
+
 
           <!-- Pagination -->
           <nav aria-label="Table Paging" class="mb-0">
