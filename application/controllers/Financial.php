2c51996a6e54bc5f8ac679f28f00fd651f665152
Akbar Anung <akbaranung06@gmail.com>
2025-06-11 11:17:35 +0700

    update

--- application/controllers/Financial.php
+++ application/controllers/Financial.php
@@ -1721,6 +1721,36 @@ class Financial extends CI_Controller
     redirect('financial/invoice');
   }
 
+  public function outstanding()
+  {
+
+    $has_access = $this->M_menu->has_access();
+
+    if (!$has_access) {
+      show_error('Forbidden Access: You do not have permission to view this page.', 403, '403 Forbidden');
+    }
+
+    $nip = $this->session->userdata('nip');
+
+    // Fetch counts
+    $result = $this->db->query("SELECT COUNT(Id) FROM memo WHERE (nip_kpd LIKE '%$nip%' OR nip_cc LIKE '%$nip%') AND (`read` NOT LIKE '%$nip%');")->row()->{'COUNT(Id)'};
+    $result2 = $this->db->query("SELECT COUNT(id) FROM task WHERE (`member` LIKE '%$nip%' or `pic` LIKE '%$nip%') AND activity='1'")->row()->{'COUNT(id)'};
+
+
+    $data = [
+      'title' => 'Outstanding',
+      'count_inbox' => $result,
+      'count_inbox2' => $result2,
+      'outstanding' => $this->M_invoice->outstanding_agent(),
+    ];
+
+    $data['pages'] = "pages/financial/v_outstanding";
+    $data['utility'] = $this->db->get('utility')->row_array();
+    $data['pages_script'] = 'script/financial/s_financial';
+    $data['menus'] = $this->M_menu->get_accessible_menus($this->session->userdata('nip'));
+    $this->load->view('index', $data);
+  }
+
   public function paid()
   {
     // print_r($_POST);
@@ -1816,6 +1846,158 @@ class Financial extends CI_Controller
       $this->session->set_flashdata('message_name', 'The invoice has been successfully void. ' . $no_inv);
       // After that you need to used redirect function instead of load view such as 
       redirect("financial/invoice");
+    }
+  }
+
+  public function list_coa()
+  {
+
+    $has_access = $this->M_menu->has_access();
+
+    if (!$has_access) {
+      show_error('Forbidden Access: You do not have permission to view this page.', 403, '403 Forbidden');
+    }
+
+    $keyword = ($this->input->post('keyword')) ? trim($this->input->post('keyword')) : (($this->session->userdata('search')) ? $this->session->userdata('search') : '');
+    if ($keyword === null) $keyword = $this->session->userdata('search');
+    else $this->session->set_userdata('search', $keyword);
+
+    $config = [
+      'base_url' => site_url('financial/list_coa'),
+      'total_rows' => $this->M_coa->count($keyword, 'v_coa_all'),
+      'per_page' => 25,
+      'uri_segment' => 3,
+      'num_links' => 10,
+      'use_page_numbers' => TRUE,
+      'enable_query_strings' => TRUE,
+      'page_query_string' => TRUE,
+      'reuse_query_string' => TRUE,
+      'query_string_segment' => 'page',
+    ];
+
+    $config['full_tag_open'] = '<ul class="pagination justify-content-end">';
+    $config['full_tag_close'] = '</ul>';
+    $config['first_link'] = "<i class='fe fe-chevrons-left'></i>";
+    $config['last_link'] = "<i class='fe fe-chevrons-right'></i>";
+    $config['first_tag_open'] = '<li class="page-item">';
+    $config['first_tag_close'] = '</li>';
+    $config['prev_link'] = "<i class='fe fe-chevron-left'></i>";
+    $config['prev_tag_open'] = '<li class="page-item">';
+    $config['prev_tag_close'] = '</li>';
+    $config['next_link'] = "<i class='fe fe-chevron-right'></i>";
+    $config['next_tag_open'] = '<li class="page-item">';
+    $config['next_tag_close'] = '</li>';
+    $config['last_tag_open'] = '<li class="page-item">';
+    $config['last_tag_close'] = '</li>';
+    $config['cur_tag_open'] = '<li class="page-item active"><a class="page-link" href="#">';
+    $config['cur_tag_close'] = '</a></li>';
+    $config['num_tag_open'] = '<li class="page-item">';
+    $config['num_tag_close'] = '</li>';
+    $config['attributes'] = array('class' => 'page-link');
+
+    $this->pagination->initialize($config);
+
+    // $page = $this->uri->segment(3) ? ($this->uri->segment(3) - 1) * $config['per_page'] : 0;
+    $page = ($this->input->get('page')) ? (($this->input->get('page') - 1) * $config['per_page']) : 0;
+    // $invoices = $this->m_invoice->list_invoice($config["per_page"], $page, $keyword);
+    $coa = $this->M_coa->list_coa_paginate($config["per_page"], $page, $keyword);
+
+    $nip = $this->session->userdata('nip');
+    $sql = "SELECT COUNT(Id) FROM memo WHERE (nip_kpd LIKE '%$nip%' OR nip_cc LIKE '%$nip%') AND (`read` NOT LIKE '%$nip%');";
+    $query = $this->db->query($sql);
+    $result = $query->row_array()['COUNT(Id)'];
+
+    $sql2 = "SELECT COUNT(id) FROM task WHERE (`member` LIKE '%$nip%' or `pic` like '%$nip%') and activity='1'";
+    $query2 = $this->db->query($sql2);
+    $result2 = $query2->row_array()['COUNT(id)'];
+
+    $data = [
+      'page' => $page,
+      'coa' => $coa,
+      'count_inbox' => $result,
+      'count_inbox2' => $result2,
+      'keyword' => $keyword,
+      'title' => "List CoA",
+    ];
+
+    $data['pages'] = "pages/financial/v_list_coa";
+    $data['utility'] = $this->db->get('utility')->row_array();
+    $data['pages_script'] = 'script/financial/s_financial';
+    $data['menus'] = $this->M_menu->get_accessible_menus($this->session->userdata('nip'));
+    $this->load->view('index', $data);
+  }
+
+
+  public function reset_coa()
+  {
+    $this->session->unset_userdata('search');
+    redirect('financial/list_coa');
+  }
+
+  public function tambahCoa()
+  {
+    $no_bb = $this->input->post('no_bb');
+    $no_sbb = $this->input->post('no_sbb');
+    $nama_coa = $this->input->post('nama_coa');
+
+    $cek_no_sbb = $this->M_coa->isAvailable('no_sbb', $no_sbb);
+    $cek_nama_coa = $this->M_coa->isAvailable('nama_perkiraan', $nama_coa);
+
+    if ($cek_no_sbb) {
+      $this->session->set_flashdata('message_error', 'No. ' . $no_sbb . ' sudah ada');
+      redirect($_SERVER['HTTP_REFERER']);
+    } else if ($cek_nama_coa) {
+      $this->session->set_flashdata('message_error', 'CoA ' . $nama_coa . ' sudah ada');
+      redirect($_SERVER['HTTP_REFERER']);
+    } else {
+
+      $substr_coa = substr($no_sbb, 0, 1);
+
+      if ($substr_coa == "1" || $substr_coa == "5" || $substr_coa == "6" || $substr_coa == "7" || $substr_coa == "5" || $substr_coa == "6") {
+        $posisi = 'AKTIVA';
+      } else {
+        $posisi = 'PASIVA';
+      }
+
+      // cek tabel
+      if ($substr_coa == "1" || $substr_coa == "2" || $substr_coa == "3") {
+        $tabel = "t_coa_sbb";
+
+        $data = [
+          'no_bb' => $no_bb,
+          'no_sbb' => $no_sbb,
+          'nama_perkiraan' => $nama_coa,
+          'posisi' => $posisi,
+          'id_cabang' => $this->session->userdata('kode_cabang'),
+        ];
+      } else if ($substr_coa == "4" || $substr_coa == "5" || $substr_coa == "6" || $substr_coa == "7" || $substr_coa == "8" || $substr_coa == "9") {
+        $tabel = "t_coalr_sbb";
+        $data = [
+          'no_lr_bb' => $no_bb,
+          'no_lr_sbb' => $no_sbb,
+          'nama_perkiraan' => $nama_coa,
+          'posisi' => $posisi,
+          'id_cabang' => $this->session->userdata('kode_cabang'),
+        ];
+      } else {
+        $this->session->set_flashdata('message_error', 'Format nomor CoA ' . $no_sbb . ' tidak sesuai.');
+        redirect($_SERVER['HTTP_REFERER']);
+      }
+
+
+      $this->cb->trans_begin();
+
+      $query = $this->cb->insert($tabel, $data);
+
+      if ($query) {
+        $this->cb->trans_commit();
+        $this->session->set_flashdata('message_name', 'CoA ' . $no_sbb . ' berhasil ditambahkan.');
+        redirect($_SERVER['HTTP_REFERER']);
+      } else {
+        $this->cb->trans_rollback();
+        $this->session->set_flashdata('message_error', 'CoA ' . $no_sbb . ' gagal disimpan. Ket:' . $this->cb->error());
+        redirect($_SERVER['HTTP_REFERER']);
+      }
     }
   }
 
