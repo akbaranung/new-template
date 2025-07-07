61f89b88f38e936234c89f1800e5ba3ab2bb2b16
xaralien <dimasuzumaki126@gmail.com>
2025-07-07 11:23:51 +0700

    Update Auth Register

--- application/views/pages/auth/v_register_progress_cabang.php
+++ application/views/pages/auth/v_register_progress_cabang.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 <link rel="stylesheet" href="<?= base_url('assets/') ?>progress-bar.css">
 
-<div class="row align-items-center h-100 w-100">
+<div class="row align-items-center h-100 w-100 m-0">
   <div class="col-lg-12 col-md-4 col-10 mx-auto">
     <div class="row">
       <div class="col-lg-3 col-md-4 col-10 mx-auto">
