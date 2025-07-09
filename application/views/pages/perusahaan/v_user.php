2aec183a9cefbbad3300f953643b50467df69a9a
xaralien <dimasuzumaki126@gmail.com>
2025-07-09 12:20:35 +0700

    update progress bar

--- application/views/pages/perusahaan/v_user.php
+++ application/views/pages/perusahaan/v_user.php
@@ -42,7 +42,7 @@
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 col-12">
       <!-- <h1 class="page-title">User List</h1> -->
       <?php
-      if ($total_user < 5) {
+      if ($total_user < 4) {
       ?>
         <!-- <div class="card shadow mb-4">
           <div class="card-header d-flex justify-content-between align-items-center">
@@ -50,50 +50,66 @@
 
           </div>
           <div class="card-body">
+
             <div class="progress">
               <div class="progress-bar" role="progressbar" style="width: <?= $percentage ?>%;" aria-valuenow="<?= $percentage ?>" aria-valuemin="0" aria-valuemax="100"><?= $percentage ?>%</div>
             </div>
           </div>
         </div> -->
+        <div class="card shadow mb-4">
+          <div class="card-header d-flex justify-content-between align-items-center">
+            <p class="card-title mb-0"><strong>Tambahkan 4 User Role (Staff, Manager, Comptroller, Direktur) untuk Memulai Hidup Baru! (<?= $total_user . '/' . $max_users_for_100_percent ?>)</strong></p>
 
-        <div class="row mb-5 mt-3">
-          <div class="container">
-            <div class="progress-container mx-auto">
-              <div class="progress" id="progress"></div>
-              <?php
-              $user_counts = isset($user_counts) ? $user_counts : [];
-              $roles = [
-
-                5 => 'Direktur',
-                3 => 'Comptroller',
-                2 => 'Manager',
-                1 => 'Staff',
-
-              ];
-              $active_fishbone = '';
-              $i = 1;
-              foreach ($roles as $value => $label) {
-                if (isset($user_counts[$value]) && $user_counts[$value] >= 1) {
-                  // $active_fishbone = 'active';
-              ?>
-                  <a href="#">
-                    <div class="circle active" data-label="<?= $label ?>"><?= $i ?></div>
-                  </a>
+          </div>
+          <div class="card-body">
+            <div class="container">
+              <div class="progress-container mx-auto">
+                <div class="progress" id="progress"></div>
                 <?php
+                $user_counts = isset($user_counts) ? $user_counts : [];
+                $roles = [
+
+                  5 => 'Direktur',
+                  3 => 'Comptroller',
+                  2 => 'Manager',
+                  1 => 'Staff',
+
+                ];
+                $i = 1;
+                $active_href = 0;
+                foreach ($roles as $value => $label) {
+                  if (isset($user_counts[$value]) && $user_counts[$value] >= 1) {
+                    // $active_fishbone = 'active';
+                ?>
+                    <a href="#">
+                      <div class="circle active" data-label="<?= $label ?>"><?= $i ?></div>
+                    </a>
+                  <?php
+                    $i++;
+                    continue;
+                  }
+
+                  if ($active_href == 0) {
+                    $active_href = 1;
+                  ?>
+                    <a href="<?= base_url('perusahaan/add_user/' . $value . '/' . $label) ?>">
+                      <div class="circle" data-label="<?= $label ?>"><?= $i ?></div>
+                    </a>
+                  <?php
+                  } else {
+                  ?>
+                    <a href="#">
+                      <div class="circle" data-label="<?= $label ?>"><?= $i ?></div>
+                    </a>
+                <?php
+                  }
                   $i++;
-                  continue;
                 }
                 ?>
-                <a href="<?= base_url('perusahaan/add_user/' . $value . '/' . $label) ?>">
-                  <div class="circle" data-label="<?= $label ?>"><?= $i ?></div>
-                </a>
-              <?php
-                $i++;
-              }
-              ?>
-              <!-- <div class="circle active" data-label="User">1</div>
+                <!-- <div class="circle active" data-label="User">1</div>
               <div class="circle" data-label="Perusahaan">2</div>
               <div class="circle" data-label="Cabang">3</div> -->
+              </div>
             </div>
           </div>
         </div>
