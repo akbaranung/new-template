9d6d468b8d4cf6f8da297f8d4599c5a7af7ba122
xaralien <dimasuzumaki126@gmail.com>
2025-07-15 14:16:21 +0700

    Register Agreement

--- application/views/pages/auth/v_register.php
+++ application/views/pages/auth/v_register.php
@@ -1,5 +1,4 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-<link href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css" rel="stylesheet" />
 <link rel="stylesheet" href="<?= base_url('assets/') ?>progress-bar.css">
 <style>
   /* Use a custom class controlled by JavaScript */
